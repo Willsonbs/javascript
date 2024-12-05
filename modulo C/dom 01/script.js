ca416fb967188be9a8a56344103705cf31dba7a1
willsonbs <willsonbs@gmail.com>
2024-12-05 10:22:18 -0300

    Anotaçoes DOM

---
+++ modulo C/dom 01/script.js
@@ -0,0 +1,16 @@
+/*
+var p1 = window.document.getElementsByTagName('p')[0]
+    window.document.write('Parágrafo: ' + p1.innerHTML)
+    p1.style.color = 'white'
+
+Por getElementsByTagName
+*/
+
+
+/*
+var d = window.document.getElementById('content')
+d.style.background = 'green'
+//d.innerText = 'Olá MUndo'
+window.document.getElementById('content').innerText = 'Olá MUndo'
+*/
+
