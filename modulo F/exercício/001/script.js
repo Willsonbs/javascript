3aafdf1bc543fb0835f66d1e700e0603915f02a3
willsonbs <willsonbs@gmail.com>
2025-01-15 19:38:35 -0300

    exercicio mod f, aula 16.

---
+++ modulo F/exercício/001/script.js
@@ -0,0 +1,51 @@
+let lista = [];
+function analyzeNumber(){
+    const numberInput = document.getElementById('numero');
+    const numero = Number(numberInput.value);
+    const select = document.getElementById('valores');
+
+    if (numero < 1 || numero > 100 || lista.includes(numero)){
+        alert('Valor inválido ou já adicionado');
+        numberInput.value = "";
+        numeroInput.focus();
+        return;
+    }
+
+    lista.push(numero);
+    const option = document.createElement('option');
+    option.text = `Valor: ${numero} adicionado`;
+    select.add(option);
+
+    numeroInput.value = "";
+    numeroInput.focus();
+}
+
+function analisar() {
+    if (lista.length === 0) {
+        alert('Adicone números para poder analisar.');
+        return;
+    }
+
+    const total = lista.length;
+    const maior = Math.max(...lista);
+    const menor = Math.min(...lista);
+    const soma = lista.reduce((acc, num) => acc + num, 0);
+    const media = soma / total;
+
+    const resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = `
+        <p>Ao todo, temos ${total} números cadastrados!</p>
+        <p>O maior valor informado foi ${maior}!</p>
+        <p>O menor valor informado foi ${menor}!</p>
+        <p>Somando todos os valores temos, ${soma}.</p>
+        <p>A média dos valores digitados é ${media.toFixed(2)}</p>
+        `
+}
+
+function limpar() {
+    lista = [];
+    document.getElementById("valores").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById("numero").innerHTML = "";
+    document.getElementById("numero").focus();
+}
