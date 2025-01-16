65f00952c2563b873526580ff3b9c87899fbdce5
willsonbs <willsonbs@gmail.com>
2025-01-16 09:37:14 -0300

    conclusão do curso de JavaScript

---
+++ modulo F/aula17/script.js
@@ -0,0 +1,17 @@
+/*
+let amigo = {
+    nome: 'josé', 
+    sexo: 'M', 
+    peso: 86.8,
+    engordar: function(p=0){
+        this.peso += p;
+    }
+};
+
+amigo.engordar(2);  
+console.log(`${amigo.nome} ${amigo.sexo} ${amigo.peso}kg`);
+*/
+
+let num = 8;
+num **= 2
+console.log(num)
